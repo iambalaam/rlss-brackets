2bbb6c62d47fe79481fb066182744726650747f0
Guy Balaam <guy.balaam@gmail.com>
2021-05-10 22:16:32 +0100

    Creating root element

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -5,7 +5,10 @@ function App() {
     return (<h1>Hello, world!</h1>);
 }
 
-const root = document.getElementById('root');
+const root = document.createElement('div');
+root.id = 'root';
+document.body.appendChild(root);
+
 render(
     <App />,
     root
