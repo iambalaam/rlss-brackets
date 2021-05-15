87904f7dacd20ac4c1798aa747a7dcb1b739f046
Guy Balaam <guy.balaam@gmail.com>
2021-05-15 14:28:04 +0100

    Ensuring socket has connected before application starts

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,19 +1,43 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { TeamEntry } from './pages/TeamEntry';
+import { io } from 'socket.io-client';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+
+
+
+
 export function App() {
+    const [isLoading, setLoading] = useState(true);
+    useEffect(function setupSocket() {
+        const socket = io();
+        (window as any).socket = socket;
+        socket.on('connect', () => {
+            console.log('connected socket');
+
+            setLoading(false);
+        })
+        return function cleanupSocket() {
+            socket.disconnect();
+        }
+    })
+
     return (
         <BrowserRouter>
             <main>
-                <Switch>
-                    <Route
-                        exact path="/:id/teams/"
-                        render={({ match }) => <TeamEntry id={match.params.id} />}
-                    />
-                    <Route><h1>404</h1></Route>
+                {
+                    isLoading
+                        ? 123
+                        : (<Switch>
+                            <Route
+                                exact path="/:id/teams/"
+                                render={({ match }) => <TeamEntry id={match.params.id} />}
+                            />
+                            <Route><h1>404</h1></Route>
 
-                </Switch>
+                        </Switch>)
+                }
             </main>
         </BrowserRouter>
     );
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { io } from 'socket.io-client';
 import { App } from './App';
 import './index.css';
 
-const socket = io();
-(window as any).socket = socket;
 
 const root = document.getElementById('root');
 
